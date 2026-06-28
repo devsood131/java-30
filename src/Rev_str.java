dea041fb32328c7bc8d84dcf62380ab411cb60d7
Dev sood <devsood84@gmail.com>
2026-06-28 15:20:42 +0530

    string buffer method

--- src/Rev_str.java
+++ src/Rev_str.java
@@ -1,14 +1,18 @@
 import java.util.*;
 public class Rev_str {
     public static void main(String[]args){
-     Scanner sc= new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
      String str=sc.nextLine();
-     String rev=" ";
-     int len=str.length();
+    /* String rev=" ";
+     // string concatination method
+     /*int len=str.length();
      for(int i=len-1;i>=0;i--){
          rev=rev+str.charAt(i);
-
          System.out.println("the reverse string is:"+" "+ rev);
-     }
+     }*/
+     // String buffer method
+        StringBuffer sb= new StringBuffer(str);
+       StringBuffer rev=sb.reverse();
+        System.out.println(rev);
     }
 }
