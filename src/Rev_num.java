1f5789f1934e868cefe580c3c798d3cee604b57e
Dev sood <devsood84@gmail.com>
2026-06-28 14:08:09 +0530

    Class names should be capital

--- src/Rev_num.java
+++ src/Rev_num.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class rev_num {
+public class Rev_num {
     public static void main(String[]args){
             Scanner sc=new Scanner(System.in);
         System.out.println("enter a number");
---
+++ src/Rev_str.java
@@ -0,0 +1,6 @@
+
+public class Rev_str {
+    public static void main(String[]args){
+
+    }
+}
