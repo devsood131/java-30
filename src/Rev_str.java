f47918e46280f9d958dd006dff8e7549ff208e57
Dev sood <devsood84@gmail.com>
2026-06-28 14:19:01 +0530

    code

--- src/Rev_str.java
+++ src/Rev_str.java
@@ -1,6 +1,14 @@
-
+import java.util.*;
 public class Rev_str {
     public static void main(String[]args){
+     Scanner sc= new Scanner(System.in);
+     String str=sc.nextLine();
+     String rev=" ";
+     int len=str.length();
+     for(int i=len-1;i>=0;i--){
+         rev=rev+str.charAt(i);
 
+         System.out.println("the reverse string is:"+" "+ rev);
+     }
     }
 }
