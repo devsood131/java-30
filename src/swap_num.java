ac333059ea4e4078aaf733780abe7f3f54d1ae1e
Dev sood <devsood84@gmail.com>
2026-06-28 10:00:16 +0530

    code

--- src/swap_num.java
+++ src/swap_num.java
@@ -1,5 +1,34 @@
 public class swap_num {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a =10;
+        int b=20;
+
+    //logic 1: Third variable
+/*        int t=a;
+        a=b;
+        b=t;
+        System.out.println("after swapping the values"+a+" "+b);      */
+
+        // logic 2 : use + & - without using third variable
+ /*     a= a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("after swapping the values"+a+" "+b);     */
+
+        //logic 3: use * & / without using third variable
+/*      a= a*b;
+        b=a/b;
+        a=a/b;
+        System.out.println("after swapping the values"+a+" "+b);      */
+
+        //logic 4: using bitwise XOR(^)
+/*      a=a^b; //10^20=30
+        b=a^b; // 30^20=10
+        a=a^b; // 30^10=20
+        System.out.println("after swapping the values"+a+" "+b);       */
+
+        //logic 5: single statement
+        b=(a+b)-(a=b);
+        System.out.println("after swapping the values"+a+" "+b);
     }
 }
